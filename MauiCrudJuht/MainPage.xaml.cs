517088590a087e4201b9d7a2f25b13bee0894ae6
Opilane TTHK <opilane@tthk.ee>
2024-05-24 15:49:38 +0300

    8_08 minute something broken

--- MauiCrudJuht/MainPage.xaml.cs
+++ MauiCrudJuht/MainPage.xaml.cs
@@ -2,19 +2,74 @@
 {
     public partial class MainPage : ContentPage
     {
+        private readonly LocalDbService _dbService;
+        private int _editCustomerId;
 
-        public MainPage()
+        public MainPage(LocalDbService dbService)
         {
             InitializeComponent();
+            _dbService = dbService;
+            Task.Run(async () => ListView.ItemsSource = await _dbService.GetCustomers());
         }
 
-        private void saveButton_Clicked(object sender, EventArgs e)
+        private async void saveButton_Clicked(object sender, EventArgs e)
         {
+            if (_editCustomerId == 0)
+            {
+                // ADD  
+                await _dbService.Create(new Customer
+                {
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text,
+                });
+            }
+            else
+            {
+                // EDIT
+
+                await _dbService.Update(new Customer
+                {
+                    Id = _editCustomerId,
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text,
+                });
+
+                _editCustomerId = 0;
+            }
+
+            nameEntryField.Text = string.Empty;
+            emailEntryField.Text = string.Empty;
+            mobileEntryField.Text = string.Empty;
+
+            ListView.ItemsSource = await _dbService.GetCustomers();
 
         }
 
-        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
+            var customer = (Customer)e.Item;
+            var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
+
+            switch (action)
+            {
+                case "Edit":
+
+                    _editCustomerId = customer.Id;
+                    nameEntryField.Text = customer.CustomerName;
+                    emailEntryField.Text = customer.Email;
+                    mobileEntryField.Text = customer.Mobile;
+
+                    break;
+                case "Delete":
+
+                    await _dbService.Delete(customer);
+                    ListView.ItemsSource = await _dbService.GetCustomers();
+
+                    break;
+            }
 
         }
     }
