4b6271377dfef3c5202692fa5192d9d9c9af12e7
Opilane TTHK <opilane@tthk.ee>
2024-05-24 14:53:03 +0300

    2_55 min in video and added pugins
    https://www.youtube-nocookie.com/embed/VziMUc-VQko?playlist=VziMUc-VQko&autoplay=1&iv_load_policy=3&loop=1&start=

---
+++ MauiCrudJuht/Customer.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MauiCrudJuht
+{
+
+    [Table("customer")]
+    public class Customer
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Email { get; set; }
+    }
+}
